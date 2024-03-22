676f7f2a3a1ab95e4b2486ca187d5e775a603450
kunxin-chor <chorkunxin@yahoo.com>
2024-03-22 03:10:27 +0000

    Added styling for css

--- 01-basics/src/App.js
+++ 01-basics/src/App.js
@@ -1,15 +1,22 @@
 // When importing your own files in the `src` folder
 // Make sure you put `./` in front
-const puppy = import("./puppy.jpg");
-const logo = import('./logo.svg');
+import puppy from "./puppy.jpg"
+import "./style.css"
 
 function App() {
   return (
     <>
         <img src={require('./puppy.jpg')}/>
-        <img src={logo}/>
+        <img src={puppy}/>
         <h1>Hello World</h1>
-        <h2>I love Chicken Rice</h2>
+        {1+1}
+        <p>
+          {Math.random() * 100 + 1}
+        </p>
+        <h2 style={{
+          'backgroundColor':'green',
+          'fontSize':'64px'
+        }}>I love Chicken Rice</h2>
         <ul>
           <li>Apples</li>
           <li>Oranges</li>
